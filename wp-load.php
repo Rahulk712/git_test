7b49b93e731633e01b20acdc538de5cc2d512808
Rahul <itsupport@baryons.net>
2022-01-10 19:22:10 +0530

    done

--- wp-load.php
+++ wp-load.php
@@ -20,7 +20,7 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
 }
-
+#########################testingh comment###################
 error_reporting( E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING | E_RECOVERABLE_ERROR );
 
 /*
